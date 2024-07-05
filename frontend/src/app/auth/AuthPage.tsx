4a793b56947ae61b396da64ebfd83ceb4552f611
Szymon Kadaś <kadasszymon@gmail.com>
2024-07-05 17:02:23 +0200

    style: auth page basic styles

--- frontend/src/app/auth/AuthPage.tsx
+++ frontend/src/app/auth/AuthPage.tsx
@@ -2,25 +2,55 @@ import { Link, useLocation } from "react-router-dom";
 import { RoutePaths } from "../../data/DataRoutes.ts";
 import styles from "./AuthPage.module.scss";
 import { AuthActionEnum } from "./utils.ts";
+import Icon from "../../components/Icon/Icon.tsx";
 
 export default function AuthPage({
   authAction,
 }: {
   authAction: AuthActionEnum;
 }) {
-  console.log(":D");
-  const { title, subtitleText, linkText, linkTo } =
+  const { title, changeActionText, linkText, linkTo } =
     getAuthPageValues(authAction);
   const location = useLocation();
   const from = location.state?.from?.pathname || RoutePaths.ROOT;
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    console.log(email, password, from);
+  }
   return (
-    <div className={styles.authWrapper}>
-      {authAction}
-      <h1>{title}</h1>
-      <p>{subtitleText}</p>
-      <Link to={linkTo}>{linkText}</Link>
-      from {from}
-    </div>
+    <>
+      <Icon src={"/icons/timberman.svg"} className={styles.icon}/>
+      <div className={styles.mainContentWrapper}>
+        <div className={styles.mainContent}>
+          <h1 className={styles.mainContentTitle}>Explore "Chuck Jokes" with us!</h1>
+          <form className={styles.mainContentForm} onSubmit={handleSubmit}>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className={styles.mainContentInput}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className={styles.mainContentInput}
+            />
+            <button
+              type="submit"
+              className={styles.mainContentButton}
+            >
+              {title}
+            </button>
+          </form>
+        </div>
+        <p className={styles.changeAuthAction}>{changeActionText} <Link to={linkTo}>{linkText}</Link></p>
+      </div>
+      <h4 className={styles.note}>"Chuck Norris can login without signing up, on any website."</h4>
+    </>
   );
 }
 
@@ -29,15 +59,14 @@ function getAuthPageValues(authAction: AuthActionEnum) {
     case AuthActionEnum.LOGIN:
       return {
         title: "Log in",
-        subtitleText: "Don't have an account?",
-        linkText: "Sign in here.",
+        changeActionText: "Don't have an account?",
+        linkText: "Sign up here.",
         linkTo: RoutePaths.REGISTRATION,
       };
-      break;
     default:
       return {
         title: "Register",
-        subtitleText: "Already have an account?",
+        changeActionText: "Already have an account?",
         linkText: "Log in here.",
         linkTo: RoutePaths.LOGIN,
       };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./app/App.tsx";
 import { AuthContextProvider } from "./contexts/AuthContext.tsx";
-import "./index.css";
+import "./index.module.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
