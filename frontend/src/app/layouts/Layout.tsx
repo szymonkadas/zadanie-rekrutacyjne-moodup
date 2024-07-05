e2f57117c0b243cdf66c9d2429d74ab15e84ac32
Szymon Kadaś <kadasszymon@gmail.com>
2024-07-05 15:42:01 +0200

    feat: layout

---
+++ frontend/src/app/layouts/Layout.tsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router";
+import Icon from "../../components/Icon/Icon.tsx";
+import useAuth from "../../hooks/useAuth.tsx";
+import styles from "./Layout.module.scss";
+
+export default function Layout() {
+  const { auth } = useAuth();
+  return (
+    <div className={styles.contentWrapper}>
+      {auth ? (
+        // <NavBar />
+        <main className={styles.mainSignedIn}>
+          <Outlet />
+        </main>
+      ) : (
+        <main className={styles.mainSignedOff}>
+          <Icon src={"/icons/gibberish.svg"} className={styles.iconBig} />
+          <Icon src={"/icons/gibberish.svg"} className={styles.iconSmall} />
+          <Outlet />
+        </main>
+      )}
+    </div>
+  );
+}
