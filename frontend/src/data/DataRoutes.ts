fd035734920d5823d52955e9cc8b2136965178b2
Szymon Kadaś <kadasszymon@gmail.com>
2024-07-05 15:43:02 +0200

    feat: data routes

---
+++ frontend/src/data/DataRoutes.ts
@@ -0,0 +1,10 @@
+export enum RoutePaths {
+  ROOT = "/",
+  NOT_FOUND = "*",
+  LOGIN = "/login",
+  REGISTRATION = "/sign-in",
+  LOGOUT = "/logout",
+  RANDOM_JOKE = "/joke/random",
+  SAVED_JOKES = "/joke/saved",
+  ADD_JOKE = "/joke/add",
+}
