cbadb47fc17433dbeda65e2eea62cc3986e87944
Szymon Kadaś <kadasszymon@gmail.com>
2024-07-06 13:36:26 +0200

    ci: integrated modules to app

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { AuthModule } from './auth/auth.module';
-// import { UsersService } from './users/users.service';
-// import { JokesModule } from './jokes/jokes.module';
-// import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersService } from './users/users.service';
+import { JokesModule } from './jokes/jokes.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  // imports: [AuthModule, JokesModule, ConfigModule.forRoot()],
-  imports: [],
+  imports: [AuthModule, JokesModule, ConfigModule.forRoot()],
   controllers: [AppController],
-  // providers: [AppService, UsersService],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
