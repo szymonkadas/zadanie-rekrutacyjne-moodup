be2f10f191d4abc822c5f410d23690b4704574ff
Szymon Kadaś <kadasszymon@gmail.com>
2024-07-06 12:14:39 +0200

    refactor: AuthPage adjusted to changes

--- frontend/src/app/auth/AuthPage.tsx
+++ frontend/src/app/auth/AuthPage.tsx
@@ -1,9 +1,19 @@
+import { ChangeEvent, Dispatch, FormEvent, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import Icon from "../../components/Icon/Icon.tsx";
+import { TextInputTypes } from "../../components/Inputs/InputType.ts";
+import TextInput from "../../components/Inputs/TextInput.tsx";
 import { RoutePaths } from "../../data/DataRoutes.ts";
+import { loginKeys } from "../../data/loginKeys.ts";
+import {
+  GenericObjectWithStrings
+} from "../../lib/utils/validationFunctions/isSuccessfulValidation.ts";
 import styles from "./AuthPage.module.scss";
 import { AuthActionEnum } from "./utils.ts";
-import Icon from "../../components/Icon/Icon.tsx";
+import { validateEmail } from "../../lib/utils/validationFunctions/validateEmail.ts";
+import { validatePassword } from "../../lib/utils/validationFunctions/validatePassword.ts";
 
+// The logic here's kinda redundant and messy but its really fast dev cycle so xd (trying to deliver in 20 hours~~)
 export default function AuthPage({
   authAction,
 }: {
@@ -11,45 +21,120 @@ export default function AuthPage({
 }) {
   const { title, changeActionText, linkText, linkTo } =
     getAuthPageValues(authAction);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessages, setErrorMessages] = useState({
+    email: "",
+    password: "",
+  });
+
   const location = useLocation();
   const from = location.state?.from?.pathname || RoutePaths.ROOT;
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement>,
+    setter: Dispatch<string>
+  ) => {
+    setter(event.target.value);
+    setErrorMessages((prevState) => ({ ...prevState, [event.target.name]: "" }));
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    console.log(email, password, from);
+
+    // in case if someone manually changes button state
+    const {errors: emailErrors, success: emailSuccess, parsedFormData: parsedEmailData} = validateEmail({email});
+    const {errors: passwordErrors, success: passwordSuccess, parsedFormData: parsedPasswordData} = validatePassword({password});
+
+    setErrorMessages((prevState) => ({ ...prevState, ...emailErrors, ...passwordErrors }));
+    if(emailSuccess && passwordSuccess) {
+      const formData = {
+        email: parsedEmailData.email,
+        password: parsedPasswordData.password
+      };
+
+      console.log(formData);
+    //   send to backend
+    //   if(AuthActionEnum.LOGIN === authAction) {
+    //   navigate(from);
+      console.log(from);
+    }
+  };
+
+  function validateField<T extends GenericObjectWithStrings>(
+    value: T,
+    validateFunction: (val: T) => {
+      success: boolean;
+      errors: T;
+      parsedFormData: T;
+    },
+    setError = true
+  ): boolean {
+    const { errors, success } = validateFunction(value);
+    setError && setErrorMessages((prevState) => ({ ...prevState, ...errors }));
+    return success;
   }
+
+  const isButtonDisabled = () => {
+    if(email === "" || password === "") {
+      return true;
+    }else{
+      const emailSuccess = validateField({email}, validateEmail, false);
+      const passwordSuccess = validateField({password}, validatePassword, false);
+      return !emailSuccess || !passwordSuccess
+    }
+  }
+
   return (
     <>
-      <Icon src={"/icons/timberman.svg"} className={styles.icon}/>
+      <Icon src={"/icons/timberman.svg"} className={styles.icon} />
       <div className={styles.mainContentWrapper}>
         <div className={styles.mainContent}>
-          <h1 className={styles.mainContentTitle}>Explore "Chuck Jokes" with us!</h1>
+          <h1 className={styles.mainContentTitle}>
+            Explore "Chuck Jokes" with us!
+          </h1>
           <form className={styles.mainContentForm} onSubmit={handleSubmit}>
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              className={styles.mainContentInput}
+            <TextInput
+              value={email}
+              type={TextInputTypes.EMAIL}
+              name={loginKeys.EMAIL}
+              onChange={(event) => handleChange(event, setEmail)}
+              onBlur={(event)=> validateField({email: event.target.value}, validateEmail)}
+              placeholder={"Type your email"}
+              label={"E-mail"}
+              initialStyle={styles.mainContentInput}
+              maxLength={100}
+              errorMessage={errorMessages.email}
             />
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              className={styles.mainContentInput}
+            <TextInput
+              value={password}
+              type={TextInputTypes.PASSWORD}
+              name={loginKeys.PASSWORD}
+              onChange={(event) => handleChange(event, setPassword)}
+              onBlur={(event)=> validateField({password: event.target.value}, validatePassword)}
+              placeholder={"Type your password"}
+              label={"Password"}
+              initialStyle={styles.mainContentInput}
+              minLength={6}
+              maxLength={50}
+              errorMessage={errorMessages.password}
             />
             <button
               type="submit"
               className={styles.mainContentButton}
+              disabled={isButtonDisabled()}
             >
               {title}
             </button>
           </form>
         </div>
-        <p className={styles.changeAuthAction}>{changeActionText} <Link to={linkTo}>{linkText}</Link></p>
+        <p className={styles.changeAuthAction}>
+          {changeActionText} <Link to={linkTo}>{linkText}</Link>
+        </p>
       </div>
-      <h4 className={styles.note}>"Chuck Norris can login without signing up, on any website."</h4>
+      <h4 className={styles.note}>
+        "Chuck Norris can login without signing up, on any website."
+      </h4>
     </>
   );
 }
